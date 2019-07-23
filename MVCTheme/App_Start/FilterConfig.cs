a8b4a32bcb9d4b7d26a6eb1d8cdfacaae66ef8df
vietle <lequocviet4990@gmail.com>
2019-07-23 23:30:48 +0700

    add
    add code

---
+++ MVCTheme/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTheme
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVCTheme/Helper/CustomViewEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTheme.Helper
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine()
+        {
+            var themeName = ConfigurationManager.AppSettings["ThemeName"];
+
+            ViewLocationFormats = new string[] { "~/Themes/" + themeName + "/Views/{1}/{0}.cshtml", "~/Themes/" + themeName + "/Views/Shared/{0}.cshtml", "~/Themes/" + themeName + "/Views/{1}/{0}.cshtml", "~/Themes/" + themeName + "/Views/Shared/{0}.cshtml" };
+            PartialViewLocationFormats = new string[] { "~/Themes/" + themeName + "/Views/{1}/{0}.cshtml", "~/Themes/" + themeName + "/Views/Shared/{0}.cshtml", "~/Themes/" + themeName + "/Views/{1}/{0}.cshtml", "~/Themes/" + themeName + "/Views/Shared/{0}.cshtml" };
+        }
+    }
+}
